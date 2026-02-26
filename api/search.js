9b39afd04a08998e41cf09d16f6bde7eda813271
trish51 <143803301+trish51@users.noreply.github.com>
2026-02-26 14:04:04 +0000

    added screenshots

--- api/search.js
+++ api/search.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
-    const apiKey = process.env.RAWG_API_KEY; // The secret key stored in Vercel
-    const { query } = req.query; // Gets the word you typed in the search bar
+    const apiKey = process.env.RAWG_API_KEY; // Replace this with your api key
+    const { query } = req.query; 
 
     const url = `https://api.rawg.io/api/games?key=${apiKey}&search=${query}`;
 
     try {
         const response = await fetch(url);
         const data = await response.json();
-        res.status(200).json(data); // Sends the game list back to your browser
+        res.status(200).json(data); 
     } catch (error) {
         res.status(500).json({ error: "API Failed" });
     }
